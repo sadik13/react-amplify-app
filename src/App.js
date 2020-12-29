b37a5d13f830f678076123e0113c9c7f5a0c5efe
sadik <hussain9533@gmail.com>
2020-12-29 20:53:07 +0530

    form input taken and given the o/p data as result

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import ReactDOM from 'react-dom';
 
 
 function App() {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+  }
+
+  myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
   return (
     <div className="App">
       <header className="App-header">
       <h2>My App content of AWS Amplify</h2>
       <Student />
       <form>
-        <h1>Hello</h1>
-        <p>Enter your name:</p>
-        <input
-          type="text"
-        />
+      <h1>Hello {this.state.username}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        onChange={this.myChangeHandler}
+      />
       </form>
       </header>
     </div>
