e715a95072e84975934bc3dcca70e4f35fda74e5
sadik <hussain9533@gmail.com>
2020-12-29 20:47:43 +0530

    updated the form data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Student  from './Student.js';
+import ReactDOM from 'react-dom';
 
 
 function App() {
@@ -9,6 +10,13 @@ function App() {
       <header className="App-header">
       <h2>My App content of AWS Amplify</h2>
       <Student />
+      <form>
+        <h1>Hello</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+        />
+      </form>
       </header>
     </div>
   );
